48f2fe1eca3d430a2e6c2dc1714e0e741a48947b
Riaj Mahmud <57424649+riajmahmud@users.noreply.github.com>
2021-10-06 13:22:23 +0600

    Add files via upload

---
+++ test/module/functions.js
@@ -0,0 +1,15 @@
+export function sum(x, y) {
+  return x + y
+}
+
+export function difference(x, y) {
+  return x - y
+}
+
+export function product(x, y) {
+  return x * y
+}
+
+export function quotient(x, y) {
+  return x / y
+}
